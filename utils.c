aa943fe44d1c26f7521700b681015fc0560bd9b4
Bernardo Jimenez Murillo <bjimenez@student.42malaga.com>
2022-08-31 14:11:38 +0200

    220831 pruebas

---
+++ utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bjimenez <bjimenez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 09:32:31 by bjimenez          #+#    #+#             */
+/*   Updated: 2022/08/31 09:33:59 by bjimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		sig;
+	int		num;
+
+	i = 0;
+	sig = 1;
+	num = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+	{
+		sig = sig * (-1);
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (str[i] - '0') + (num * 10);
+		i++;
+	}
+	return (num * sig);
+}
