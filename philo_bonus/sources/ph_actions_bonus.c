b34ab89081928204ebd4d2ace5b7fba5694b37e5
Bernardo Jimenez <bjimenez@MacBook-Pro-de-Berna.local>
2022-09-19 22:58:46 +0200

    220919 2

--- philo_bonus/sources/ph_actions_bonus.c
+++ philo_bonus/sources/ph_actions_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bjimenez <bjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 13:33:26 by bjimenez          #+#    #+#             */
-/*   Updated: 2022/09/18 20:45:21 by bjimenez         ###   ########.fr       */
+/*   Updated: 2022/09/19 22:39:07 by bjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	ft_thinking(t_data_philo *g_dat, long int now)
 {
 	(void) now;
+	sem_wait(g_dat->sem_eat);
 	sem_wait(g_dat->in_arg->sem_wr);
 	printf("%ld %d is thinking\n", ft_timenow() - g_dat->start,
 		g_dat->n_philo + 1);
 	sem_post(g_dat->in_arg->sem_wr);
+	sem_post(g_dat->sem_eat);
 }
 
 void	ft_eating(t_data_philo *g_dat, long int now)
@@ -35,15 +37,16 @@ void	ft_eating(t_data_philo *g_dat, long int now)
 	time = now + g_dat->in_arg->t_toeat;
 	while (time >= ft_timenow())
 		usleep(300);
+	sem_post(g_dat->in_arg->sem_fork);
+	sem_post(g_dat->in_arg->sem_fork);
 	sem_post(g_dat->sem_eat);
-	sem_post(g_dat->in_arg->sem_fork);
-	sem_post(g_dat->in_arg->sem_fork);
 }
 
 void	ft_sleeping(t_data_philo *g_dat, long int now)
 {
 	long int		time;
 
+	sem_wait(g_dat->sem_eat);
 	sem_wait(g_dat->in_arg->sem_wr);
 	printf("%ld %d is sleeping\n", ft_timenow() - g_dat->start,
 		g_dat->n_philo + 1);
@@ -51,5 +54,6 @@ void	ft_sleeping(t_data_philo *g_dat, long int now)
 	time = now + g_dat->in_arg->t_sleep;
 	while (time >= ft_timenow())
 		usleep(300);
+	sem_post(g_dat->sem_eat);
 	ft_thinking(g_dat, time);
 }
